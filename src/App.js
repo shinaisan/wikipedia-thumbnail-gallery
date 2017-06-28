1de087662b7f1c3f4edd3dd315fee5d5b83bc2fa
shinaisan <shinaisan@users.noreply.github.com>
2017-06-28 22:09:23 +0900

    Stop using class name modal.

--- src/App.js
+++ src/App.js
@@ -148,22 +148,23 @@ const Modal = ({ match, history }) => {
         background: 'rgba(0, 0, 0, 0.15)'
       }}
     >
-      <div className='modal' style={{
-        position: 'absolute',
-        background: '#fff',
-        top: 25,
-        left: '10%',
-        right: '10%',
-        padding: 15,
-        border: '2px solid #444'
-      }}>
-      <h1>{image.title}</h1>
-      <Image color={image.color} />
-      <button type='button' onClick={back}>
-        Close
-      </button>
+      <div className='modal-image'
+        style={{
+          position: 'absolute',
+          background: '#fff',
+          top: 25,
+          left: '10%',
+          right: '10%',
+          padding: 15,
+          border: '2px solid #444'
+        }}>
+        <h1>{image.title}</h1>
+        <Image color={image.color} />
+        <button type='button' onClick={back}>
+          Close
+        </button>
+      </div>
     </div>
-  </div>
   )
 }
 
